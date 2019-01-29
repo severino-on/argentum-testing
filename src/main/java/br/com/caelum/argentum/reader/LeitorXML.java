2539269acb3a883d79d4f072024b34cd7aa15340
severino-on <43893211+severino-on@users.noreply.github.com>
2019-01-29 17:22:02 -0200

    Update LeitorXML.java
    ...

--- src/main/java/br/com/caelum/argentum/reader/LeitorXML.java
+++ src/main/java/br/com/caelum/argentum/reader/LeitorXML.java
@@ -12,7 +12,7 @@ public class LeitorXML {
 
 	@SuppressWarnings("unchecked")
 	public List<Negociacao> carrega(InputStream inputStream) {
-		XStream_ stream = new XStream(new DomDriver());
+		XStream stream = new XStream(new DomDriver());
 		stream.alias("negociacao", Negociacao.class);
 		return (List<Negociacao>) stream.fromXML(inputStream);
 	}
